8fc0adaba75514e62d0f475091b28906c9fe83a9
Font-end <0306211480@caothang.edu.vn>
2023-10-27 22:41:20 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import TrangChu4 from './pages/trang_chu_4';
+
+import './style.css';
+
+import store from './store'
+import { Provider } from 'react-redux'
+
+function App() {
+  return (
+    <>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/demo-4" element={<TrangChu4/>}/>
+      </Routes>
+    </Provider>
+    </>
+  );
+}
+
+export default App;
